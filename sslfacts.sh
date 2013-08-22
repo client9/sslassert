11ac6457498ced1db64e647b9842c3404bd87b44
Nick Galbreath <nickg@client9.com>
2013-08-22 16:37:41 +0900

    fix up CLI

--- sslfacts.sh
+++ sslfacts.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 export SSLASSERT_DEBUG=1
-export HOSTPORT="${1}:443"
 
+export HOSTPORT="${1}"
+if [[ "${HOSTPORT}" != *:* ]]; then
+    export HOSTPORT="${HOSTPORT}:443"
+fi
+
+else:
 # overide using env variable
 # export URLPATH=/
-
+#export URLPATH="/\n"
 
 source ./sslassert.sh
 
